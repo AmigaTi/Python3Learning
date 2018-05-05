25576101f9ce0e2c09996b4653f846c4ecf480bc
shellever <shellever@163.com>
2018-05-06 07:00:48 +0800

    Tkinter - Widgets - Radiobutton - change window's title

--- interface/ui-tkinter-widgets/tkinter-radiobutton.py
+++ interface/ui-tkinter-widgets/tkinter-radiobutton.py
@@ -7,9 +7,9 @@ from tkinter import *
 # Radiobutton - 单选按钮
 
 
-root = Tk()                 # 初始化TK()
-root.title('Label')         # 设置窗口标题
-root.geometry('300x200')    # 设置窗口大小
+root = Tk()                                 # 初始化TK()
+root.title('Tkinter - Radiobutton')         # 设置窗口标题
+root.geometry('300x200')                    # 设置窗口大小
 root.resizable(width=False, height=True)    # 设置窗口的长宽是否可变
 
 
