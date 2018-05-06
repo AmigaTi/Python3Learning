3e7014655c1b3903fdbb80452e9f3ab3b4ea9803
shellever <shellever@163.com>
2018-05-06 20:47:51 +0800

    Tkinter - Widgets - OptionMenu

--- interface/ui-tkinter-widgets/tkinter-optionmenu.py
+++ interface/ui-tkinter-widgets/tkinter-optionmenu.py
@@ -8,11 +8,12 @@ root.title('Tkinter - OptionMenu')          # 设置窗口标题
 root.geometry('300x200')                    # 设置窗口大小
 root.resizable(width=False, height=True)    # 设置窗口的长宽是否可变
 
-v = StringVar(root)
+v = StringVar()
 v.set('Python')             # 设置默认值
 option_menu = OptionMenu(root, v, 'Python', 'Perl', 'Java')
-#lang = ['Python', 'Perl', 'Java']
-#option_menu = apply(OptionMenu, (root, v) + tuple(lang))
+# lang = ['Python', 'Perl', 'Java']
+# apply()函数在Python3中已经不支持
+# option_menu = apply(OptionMenu, (root, v) + tuple(lang))
 option_menu.pack()
 
 
@@ -22,6 +23,13 @@ def print_option(event):
 
 # 绑定OptionMenu的点击事件回调函数
 option_menu.bind('<Button-1>', print_option)
+
+
+#
+options = tuple(range(0, 10))
+v2 = StringVar()
+v2.set(options[0])
+OptionMenu(root, v2, *options).pack()   # *号有一个解包的功能
 
 
 root.mainloop()             # 进入消息循环
