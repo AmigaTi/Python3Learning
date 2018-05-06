f6c8974b67f2c6b23e5573a5f714be85cd821180
shellever <shellever@163.com>
2018-05-06 22:47:11 +0800

    Tkinter - Geometry - Grid modified

--- interface/ui-tkinter-geometry/geometry-grid.py
+++ interface/ui-tkinter-geometry/geometry-grid.py
@@ -13,18 +13,40 @@ from tkinter import *
 # 相关属性
 # row     指定行号
 # column  指定列号
+# rowspan     横跨几行
+# columnspan  横跨几列
 
 
 root = Tk()
+root.title('Sign in')
+# root.geometry('300x200')
+
+
+def do_sign_in():
+    username = entry_username.get()
+    password = entry_password.get()
+    if username == 'shellever' and password == '123456':
+        label_tip['text'] = 'sign in success'
+    else:
+        label_tip['text'] = 'username or password error'
+        entry_username.delete(0, len(username))
+        entry_password.delete(0, len(password))
+
 
 Label(root, text='Username').grid(row=0, sticky=W)
 Label(root, text='Password').grid(row=1, sticky=W)
 
-Entry(root).grid(row=0, column=1, sticky=E)
-Entry(root).grid(row=1, column=1, sticky=E)
+entry_username = Entry(root)
+entry_username.grid(row=0, column=1, sticky=E)
+entry_password = Entry(root)
+entry_password['show'] = '*'
+entry_password.grid(row=1, column=1, sticky=E)
 
 # span [column, columnspan) = [0, 2)
-Button(root, text='Login').grid(row=2, column=0, sticky=EW, columnspan=2)
+Button(root, text='Login', command=do_sign_in).grid(row=2, column=0, sticky=EW, columnspan=2, padx=2, pady=2)
+
+label_tip = Label(root)
+label_tip.grid(row=3, column=0, columnspan=2)
 
 root.mainloop()
 
