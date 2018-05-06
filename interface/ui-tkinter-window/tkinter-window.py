b51bd8fe255738b4dd2a2c6e5f241236be7c1917
shellever <shellever@163.com>
2018-05-06 23:59:39 +0800

    Tkinter - Window - icon and so on.

--- interface/ui-tkinter-window/tkinter-window.py
+++ interface/ui-tkinter-window/tkinter-window.py
@@ -24,10 +24,12 @@ def centralize_window(root, ww, wh):
     root.geometry("%dx%d+%d+%d" % (ww, wh, x, y))
 
 
-root = tk.Tk()                           # 初始化TK()
-root.title('Tkinter - Window')           # 设置窗口标题
-# root.geometry()                          # 设置窗口大小
-centralize_window(root, 600, 400)        # 窗口于屏幕居中
+root = tk.Tk()                                  # 初始化TK()
+root.title('Tkinter - Window')                  # 设置窗口标题
+# root.geometry('600x400')                      # 设置窗口大小
+# root.overrideredirect(1)                      # 去除边框
+root.iconbitmap('PowerPoint_Letter_64px.ico')   # 设置标题栏的默认图标
+centralize_window(root, 600, 400)               # 窗口于屏幕居中
 
 # root.maxsize(600, 400)                 # 设置最大化窗口大小
 # root.minsize(300, 240)                 # 设置最小化窗口大小
