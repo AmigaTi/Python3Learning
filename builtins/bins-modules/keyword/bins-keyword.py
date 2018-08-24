90f7251b7da2dc4e5c904b813893fe15fc5412a1
shellever <shellever@163.com>
2018-08-25 05:19:55 +0800

    builtin module - keyword

---
+++ builtins/bins-modules/keyword/bins-keyword.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import keyword
+
+print(keyword.kwlist)
+
+"""
+['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 
+'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 
+'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 
+'return', 'try', 'while', 'with', 'yield']
+"""
