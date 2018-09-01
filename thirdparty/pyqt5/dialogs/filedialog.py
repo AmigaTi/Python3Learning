603a2eb1db1d37a78386125e48b8fb74bd22c20d
shellever <shellever@163.com>
2018-09-02 05:40:37 +0800

    FileDialog

---
+++ thirdparty/pyqt5/dialogs/filedialog.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QAction
+from PyQt5.QtWidgets import QTextEdit
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtGui import QIcon
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.textEdit = QTextEdit()
+        self.setCentralWidget(self.textEdit)
+        self.statusBar()
+
+        openFile = QAction(QIcon('document_open_32px.png'), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open new file')
+        openFile.triggered.connect(self.showFileDialog)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('FileDialog')
+        self.show()
+
+    def showFileDialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+        if fname[0]:
+            f = open(fname[0], 'r')
+            with f:
+                data = f.read()
+                self.textEdit.setText(data)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    sys.exit(app.exec_())
